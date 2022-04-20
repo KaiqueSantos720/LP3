cfb665f7a3835d4a8bf89cda422f4a5f760ba8f3
Kaique Santos <kaique.santos20042018@gmail.com>
2022-04-20 15:29:25 -0300

    Classe Conta adicionada

---
+++ ProjetoConta/Program.cs
@@ -0,0 +1,15 @@
+﻿Conta c1 = new Conta(1);
+Conta c2 = new Conta(2);
+
+c1.Depositar(500.00);
+c1.Sacar(200.00);
+Console.WriteLine("Saldo c1: {0}", c1.Saldo);
+
+c2.Depositar(200.00);
+c2.Sacar(150.00);
+Console.WriteLine("Saldo c2: {0}", c2.Saldo);
+
+c1.Transferir(300.00, c2);
+Console.WriteLine("Saldo c1: {0}", c1.Saldo);
+Console.WriteLine("Saldo c2: {0}", c2.Saldo);
+
---
+++ ProjetoConta/README.md
@@ -0,0 +1,21 @@
+# Projeto Classe Conta
+
+- Aplicação do console que executa os métodos sacar, depositar e transferir
+
+## Funcionalidades
+
+- Sacar valores
+- Transferir valores
+- Depositar valores
+
+## Tecnologias Utiizadas
+
+- dotnet 6
+
+## Como Executar
+
+- Realize o clone do projeto no repositório e execute o comando
+
+```
+dotnet run
+```
